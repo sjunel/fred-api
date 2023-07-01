ebb59aeba8da8fa76eb61e8f3e6bd62cd3fc366b
Sarah J Lee <44482222+sjunel@users.noreply.github.com>
2023-06-30 20:15:33 -0700

    Set up FRED API obj, dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+import Fred from 'node-fred';
 
 const app: Application = express();
+dotenv.config();
+const fred: Fred = new Fred(process.env.FRED_API_KEY ?? '');
 
-const PORT: number = 3001;
+const PORT: number = parseInt(process.env.PORT ?? '3000');
 
 app.use('/', (req: Request, res: Response): void => {
   res.send('Hello world!');
