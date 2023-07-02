5eaafca8e5834752449c9b2385f341e9f1b4ce6c
Sarah J Lee <44482222+sjunel@users.noreply.github.com>
2023-07-01 23:24:12 -0700

    Add example API call using fetch

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,26 @@ import Fred from 'node-fred';
 
 const app: Application = express();
 dotenv.config();
+
 const fred: Fred = new Fred(process.env.FRED_API_KEY ?? '');
 
 const PORT: number = parseInt(process.env.PORT ?? '3000');
 
-app.use('/', (req: Request, res: Response): void => {
-  res.send('Hello world!');
+app.use('/', async (req: Request, res: Response): Promise<void> => {
+  try {
+  // const series = await fred.series.getSeries('SP500');
+
+  const URL = `https://api.stlouisfed.org/fred/series?series_id=SP500&api_key=${process.env.FRED_API_KEY}&file_type=json`;
+  const response = await fetch(URL);
+  const series = await response.json();
+
+  res.send(series);
+
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.listen(PORT, ():void => {
+app.listen(PORT, (): void => {
   console.log(`Listening on port ${PORT}...`);
 });
