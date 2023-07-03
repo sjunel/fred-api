f41fc407ff4279432264c3f9b1cb12f497f6a854
Sarah J Lee <44482222+sjunel@users.noreply.github.com>
2023-07-02 22:58:46 -0700

    Update dependency, example API call

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,8 @@ const PORT: number = parseInt(process.env.PORT ?? '3000');
 
 app.use('/', async (req: Request, res: Response): Promise<void> => {
   try {
-  // const series = await fred.series.getSeries('SP500');
-
-  const URL = `https://api.stlouisfed.org/fred/series?series_id=SP500&api_key=${process.env.FRED_API_KEY}&file_type=json`;
-  const response = await fetch(URL);
-  const series = await response.json();
-
-  res.send(series);
-
+    const series = await fred.series.getSeries('SP500');
+    res.send(series);
   } catch (err) {
     res.send(err);
   }
