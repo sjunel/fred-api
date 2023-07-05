e47dd295abe6eb2648b7fed95a37b1327fc835b7
Sarah J Lee <44482222+sjunel@users.noreply.github.com>
2023-07-05 04:59:37 -0700

    Add data viz library, example plot, separate endpoint for getting FRED data, func for fetching data from server. WIP format pulled data.

--- src/app.ts
+++ src/app.ts
@@ -12,25 +12,19 @@ const PORT: number = parseInt(process.env.PORT ?? '3000');
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.get('/', async (req: Request, res: Response): Promise<void> => {
+app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response): void => {
+  res.render('home');
+});
+
+app.get('/data', async (req: Request, res: Response): Promise<void> => {
   try {
-    // TODO check error for static type checking with wrapper
-    // const series = await fred.series.getObservationsForSeries('SP500');
+    const series = await fred.series.getObservationsForSeries('SP500');
 
-    // temp workaround, directly pull data
-    const URL = `https://api.stlouisfed.org/fred/series/observations?series_id=SP500&api_key=${process.env.FRED_API_KEY}&file_type=json`;
-    const response = await fetch(URL);
-    const series = await response.json();
+    // TODO format pulled data
 
-    // TODO check error for dynamic import() within commmonjs
-    // const plot = Plot.plot({
-    //   marks: [
-    //     Plot.lineY(series.observations, { x: 'date', y: 'value' })
-    //   ]
-    // });
-
-    // TODO temp workaround to load charting lib via CDN
-    res.render('home', { data: JSON.stringify(series.observations) });
+    res.send(series.observations);
   } catch (err) {
     res.send(err);
   }
