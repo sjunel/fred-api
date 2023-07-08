e19439c783197841be15e93796f24ab3acd2ddab
Sarah J Lee <44482222+sjunel@users.noreply.github.com>
2023-07-08 12:35:49 -0700

    Update dependencies, API call

--- src/app.ts
+++ src/app.ts
@@ -20,13 +20,7 @@ app.get('/', (req: Request, res: Response): void => {
 
 app.get('/data', async (req: Request, res: Response): Promise<void> => {
   try {
-    // TODO check error for static type checking with wrapper
-    // const series = await fred.series.getObservationsForSeries('SP500');
-
-    // temp workaround, directly pull data
-    const URL = `https://api.stlouisfed.org/fred/series/observations?series_id=SP500&api_key=${process.env.FRED_API_KEY}&file_type=json`;
-    const response = await fetch(URL);
-    const series = await response.json();
+    const series = await fred.series.getObservationsForSeries('SP500');
 
     let data = series.observations.filter((obj: any) => {
       function isValidDate(dateStr: string) {
